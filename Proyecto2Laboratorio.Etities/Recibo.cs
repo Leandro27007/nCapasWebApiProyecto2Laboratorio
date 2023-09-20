ce3b70ee634ff190ebc79c2350e5c82adc081bf2
Masuel Matos <masuelm04@gmail.com>
2023-09-20 19:00:32 -0400

    CAMBIOS DE LAS CLASES ENTITIES

--- Proyecto2Laboratorio.Etities/Recibo.cs
+++ Proyecto2Laboratorio.Etities/Recibo.cs
@@ -9,11 +9,5 @@ namespace Proyecto2Laboratorio.Entities
 {
     public class Recibo
     {
-        [Key]
-        public int ReciboID { get; set; }
-        [Required]
-        public string NCF { get; set; }
-        public decimal Descuento { get; set; }
-        public DateTime Fecha { get; set; }
     }
 }
