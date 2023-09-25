cf79ac55cbda998676c97dbce57c7dc456b08ebf
HighDrizzDev <usvirux@gmail.com>
2023-09-25 18:33:32 -0400

    TestRefactorizacion

---
+++ Proyecto2Laboratorio.Entities/TurnoPruebaDeLaboratorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2Laboratorio.Entities
+{
+    public class TurnoPruebaDeLaboratorio
+    {
+        public int TurnoPruebaDeLaboratorioId { get; set; }
+
+
+
+
+
+    }
+}
