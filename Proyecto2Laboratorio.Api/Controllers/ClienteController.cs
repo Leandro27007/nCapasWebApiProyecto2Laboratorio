df4cffcd01e6f2f0aa280408bc4ee465ee15d7d4
Masuel Matos <masuelm04@gmail.com>
2023-10-06 17:12:36 -0400

    Método CrearCliente agregado a ClienteController

---
+++ Proyecto2Laboratorio.Api/Controllers/ClienteController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto2Laboratorio.Entities;
+
+namespace Proyecto2Laboratorio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+
+        private readonly IClienteService _clienteService;
+
+        public ClienteController(IClienteService clienteService)
+        {
+
+            this._clienteService = clienteService;
+
+        }
+
+        [HttpPost ("CrearCliente")]
+        public ActionResult CrearCliente([FromBody]Cliente modelo)
+        {
+            try
+            {
+                var cliente = _clienteService.CrearClienteAsync(modelo);
+                return Ok(cliente);
+
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest("Ocurrio un error, no se creo el cliente");
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
