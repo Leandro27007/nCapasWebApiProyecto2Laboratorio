1f95c2f6adb855fb1c2dbc43b5b91352feefc2c3
Lean <Lean@DESKTOP-Q7BF4LS>
2023-10-28 14:22:27 -0400

    SwaggerModoDesarrollo

--- Proyecto2Laboratorio.Api/Program.cs
+++ Proyecto2Laboratorio.Api/Program.cs
@@ -55,7 +55,7 @@ var app = builder.Build();
 app.UseCors();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
